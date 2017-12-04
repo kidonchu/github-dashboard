04e79cc30fa47703f256d4df222f424be6a69007
Kidon Chu <kidonchu@gmail.com>
2017-12-04 15:28:27 -0600

    Fix pull serializer find hasMany issue

--- app/serializers/pull.js
+++ app/serializers/pull.js
@@ -6,49 +6,51 @@ function trimHost(str) {
 
 export default DS.JSONAPISerializer.extend({
 
-	normalizeResponse(store, primaryModelClass, payload, id, requestType) {
+	normalizeFindHasManyResponse(store, primaryModelClass, payload, id, requestType){
+		let jsonPayload = {
+			data: payload.map(this.doNormalize)
+		};
+		return this._super(store, primaryModelClass, jsonPayload, id, requestType);
+	},
+
+	doNormalize(payload) {
 
 		let repo = payload.base.repo;
 		let user = payload.user;
 
-		let jsonPayload = {
-			data: {
-				id: `${repo.name}-${payload.number}`,
-				type: 'pull',
-				attributes: {
-					number: payload.number,
-					title: payload.title,
-					body: payload.body,
-					'html-url': payload.html_url,
-					state: payload.state,
+		return {
+			id: `${repo.name}-${payload.number}`,
+			type: 'pull',
+			attributes: {
+				number: payload.number,
+				title: payload.title,
+				body: payload.body,
+				'html-url': payload.html_url,
+				state: payload.state,
+			},
+			relationships: {
+				author: {
+					data: {
+						type: 'user',
+						id: user.login,
+					}
 				},
-				relationships: {
-					author: {
-						data: {
-							type: 'user',
-							id: user.login,
-						}
-					},
-					reviews: {
-						links: {
-							related: trimHost(payload.url) + '/reviews',
-						}
-					},
-					'issue-comments': {
-						links: {
-							related: trimHost(payload.comments_url),
-						}
-					},
-					'review-comments': {
-						links: {
-							related: trimHost(payload.review_comments_url),
-						}
+				reviews: {
+					links: {
+						related: trimHost(payload.url) + '/reviews',
+					}
+				},
+				'issue-comments': {
+					links: {
+						related: trimHost(payload.comments_url),
+					}
+				},
+				'review-comments': {
+					links: {
+						related: trimHost(payload.review_comments_url),
 					}
 				}
 			}
 		};
-
-		return this._super(store, primaryModelClass, jsonPayload, id, requestType);
 	},
-
 });
